d2b3339a6b4419170f66208e51387bdd22e7252e
Kunal Kishor <kunal.kishor@mindtree.com>
2022-11-24 08:43:52 +0000

    Testing

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return '<h1>Playing with a Flask Application using Cloud Build at '+ str(datetime.datetime.now()) +'!</h1>'
+    return '<h1>Playing with a Flask Application 123 using Cloud Build at '+ str(datetime.datetime.now()) +'!</h1>'
 
 
 @app.route('/about/')
